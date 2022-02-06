6c53f527424392b59cb6d3582901e684d12fa5a2
Anjali Gopinathan <asgopina@usc.edu>
2022-02-06 06:11:07 -0800

    minor enable characteristic

--- androidapp/ble-starter-android-master/app/src/main/java/com/punchthrough/blestarterappandroid/CharacteristicAdapter.kt
+++ androidapp/ble-starter-android-master/app/src/main/java/com/punchthrough/blestarterappandroid/CharacteristicAdapter.kt
@@ -40,33 +40,33 @@ class CharacteristicAdapter(
         // use an enum to map 0/1/2 back to "unripe", "perfectly ripe", or "overripe"
         // Print this value to a string attached to the "result" textview
 
-//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-//        val view = parent.context.layoutInflater.inflate(
-//            R.layout.row_characteristic,
-//            parent,
-//            false
-//        )
-//        return ViewHolder(view, onClickListener)
-//    }
-//
-//    override fun getItemCount() = items.size
-//    val instance = listOf<String>()
-//    fun getInstances() = characteristic.printProperties()
-//
-//    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-//        val item = items[position]
-//        holder.bind(item)
-//    }
-//
-//    class ViewHolder(
-//        private val view: View,
-//        private val onClickListener: ((characteristic: BluetoothGattCharacteristic) -> Unit)
-//    ) : RecyclerView.ViewHolder(view) {
-//
-//        fun bind(characteristic: BluetoothGattCharacteristic) {
-//            view.characteristic_uuid.text = characteristic.uuid.toString()
-//            view.characteristic_properties.text = characteristic.printProperties()
-//            view.setOnClickListener { onClickListener.invoke(characteristic) }
-//        }
-//    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = parent.context.layoutInflater.inflate(
+            R.layout.row_characteristic,
+            parent,
+            false
+        )
+        return ViewHolder(view, onClickListener)
+    }
+
+    override fun getItemCount() = items.size
+    val instance = listOf<String>()
+    fun getInstances() = characteristic.printProperties()
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = items[position]
+        holder.bind(item)
+    }
+
+    class ViewHolder(
+        private val view: View,
+        private val onClickListener: ((characteristic: BluetoothGattCharacteristic) -> Unit)
+    ) : RecyclerView.ViewHolder(view) {
+
+        fun bind(characteristic: BluetoothGattCharacteristic) {
+            view.characteristic_uuid.text = characteristic.uuid.toString()
+            view.characteristic_properties.text = characteristic.printProperties()
+            view.setOnClickListener { onClickListener.invoke(characteristic) }
+        }
+    }
 }
